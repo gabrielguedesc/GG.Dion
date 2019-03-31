b2b5f042dbd7f089940a7ec2e3298ee257b193a9
Gabriel Guedes Correa <g4brielguedes@gmail.com>
2019-03-31 01:44:04 -0300

    Add: IoC project

---
+++ src/GG.Dion.Infra.IoC/NativeInjectorBootStrapper.cs
@@ -0,0 +1,22 @@
+﻿using GG.Dion.Domain.Interfaces.Repositories;
+using GG.Dion.Domain.Interfaces.Services;
+using GG.Dion.Domain.Services;
+using GG.Dion.Infra.Data.Context;
+using GG.Dion.Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GG.Dion.Infra.IoC
+{
+    public static class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            // Domain
+            services.AddScoped<ICustomerService, CustomerService>();
+
+            // Infra - Data
+            services.AddScoped<DionProjectContext>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
